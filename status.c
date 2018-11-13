017627677b9712b15e8b78ac9d1b231f82aa1572
opask <opask@cock.li>
2018-11-12 23:08:06 -0700

    make use of buf in mpd()

--- status.c
+++ status.c
@@ -108,7 +108,6 @@ mpd(enum mpd_tag_type type)
 	struct mpd_connection *conn = NULL;
 	struct mpd_song *song = NULL;
 	struct mpd_status *status = NULL;
-	char *ret = NULL;
 
 	conn = mpd_connection_new("localhost", 0, 600);
 	if (!conn || mpd_connection_get_error(conn))
@@ -125,15 +124,16 @@ mpd(enum mpd_tag_type type)
 	if (status && (mpd_status_get_state(status) >= MPD_STATE_PLAY)) {
 		mpd_response_next(conn);
 		song = mpd_recv_song(conn);
-		ret = smprintf("%s", mpd_song_get_tag(song, type, 0));
+		snprintf(buf, sizeof(buf), "%s",
+			mpd_song_get_tag(song, type, 0));
 		mpd_song_free(song);
 	} else
-		ret = smprintf("");
+		snprintf(buf, sizeof(buf), "%s", "");
 
 	mpd_response_finish(conn);
 	mpd_connection_free(conn);
 
-	return ret;
+	return smprintf("%s", buf);
 }
 
 int
@@ -142,8 +142,7 @@ main(void)
 	struct sigaction sa;
 	char *status;
 	char *time;
-	char *song;
-	char *artist;
+	char *artist, *song;
 	long vol;
 
 	memset(&sa, 0, sizeof(sa));
