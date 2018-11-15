043dd572a7ca9d3298bf81d2d2646ba760f1f6a2
opask <opask@cock.li>
2018-11-14 23:29:27 -0700

    make config.mk more openbsd friendly

--- config.mk
+++ config.mk
@@ -1,7 +1,11 @@
 PREFIX = /
 
-LIBS = -lmpdclient -lX11 -lasound
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+INCS = -I$(X11INC) -I/usr/local/include
+LIBS = -L$(X11LIB) -L/usr/local/lib -lmpdclient -lX11 -lasound
 
 CPPFLAGS = -D_POSIX_C_SOURCE
-CFLAGS = -O2 -std=c99 -Wall -pedantic $(CPPFLAGS)
+CFLAGS = -O2 -std=c99 -Wall -pedantic $(CPPFLAGS) $(INCS)
 LDFLAGS = $(LIBS)
