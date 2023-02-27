db56d3ba6b063539f862c5de941db66820e5a864
Randy Palamar <palamar@ualberta.ca>
2023-02-27 10:26:06 -0700

    increase _POSIX_C_SOURCE define and add comment about optional libs
    _POSIX_C_SOURCE=200809L: needed for some signal(7) flags which are
    supposed to be exposed in C99 but that glibc hides

--- config.mk
+++ config.mk
@@ -2,8 +2,11 @@ SRC =\
 	blocks/date.c\
 	blocks/linux/battery.c\
 	blocks/linux/blight.c\
-	blocks/linux/volume.c\
-	blocks/mpd.c
+	blocks/script.c
+# blocks/linux/volume.c: needs: -lasound
+#	blocks/linux/volume.c
+# blocks/mpd.c: needs: -lmpdclient
+#	blocks/mpd.c
 
 PREFIX = /
 
@@ -11,8 +14,8 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 INCS = -I$(X11INC) -I/usr/local/include
-LIBS = -L$(X11LIB) -L/usr/local/lib -lmpdclient -lX11 -lasound
+LIBS = -L$(X11LIB) -L/usr/local/lib -lX11
 
-CPPFLAGS = -D_POSIX_C_SOURCE
+CPPFLAGS = -D_POSIX_C_SOURCE=200809L
 CFLAGS = -O2 -std=c99 -Wall -pedantic
 LDFLAGS = -s
