7ea779050b0a9e71c8d95cf1c8e085c0c49c8d43
Randy Palamar <randy@rnpnr.xyz>
2024-12-19 19:11:55 -0700

    drop util.c
    no need for a tiny useless file like this

--- status.c
+++ status.c
@@ -1,5 +1,6 @@
 /* See LICENSE for license details. */
 #include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,7 +31,35 @@ static int dflag = 0;
 static sigset_t blocksigmask;
 static struct Block *dirty;
 
-#include  "util.c"
+static void
+die(const char *errstr, ...)
+{
+	va_list ap;
+
+	va_start(ap, errstr);
+	vfprintf(stderr, errstr, ap);
+	va_end(ap);
+	exit(1);
+}
+
+static int
+pscanf(const char *path, const char *fmt, ...)
+{
+	FILE *fp;
+	va_list ap;
+	int ret;
+
+	if (!(fp = fopen(path, "r")))
+		return -1;
+
+	va_start(ap, fmt);
+	ret = vfscanf(fp, fmt, ap);
+	va_end(ap);
+	fclose(fp);
+
+	return (ret == EOF) ? -1 : ret;
+}
+
 #include "config.h"
 
 static void
